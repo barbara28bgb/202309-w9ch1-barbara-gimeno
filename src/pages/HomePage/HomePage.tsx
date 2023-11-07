3f3fcf922a578712471eafd625bc437eda1d3df7
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 18:57:12 +0100

    Add dispatcher to HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import useMoviesApi from "../../hooks/useMoviesApi";
+import { useAppDispatch } from "../../store/hooks";
+import { loadMoviesActionCreator } from "../../store/features/movieSlice/movieSlice";
+import MoviesList from "../../components/MoviesList/MoviesList";
+import HomePageStyled from "./HomePageStyled";
+
 const HomePage = (): React.ReactElement => {
-  return <h1 className="homepage__title">Top Movies</h1>;
+  const dispatch = useAppDispatch();
+  const { getMovies } = useMoviesApi();
+
+  useEffect(() => {
+    (async () => {
+      const actualMovies = await getMovies();
+      dispatch(loadMoviesActionCreator(actualMovies));
+    })();
+  }, [dispatch, getMovies]);
+
+  return (
+    <HomePageStyled className="home-page">
+      <h1 className="home-page__title">Best Movies of the last 50 years </h1>
+      <main>
+        <MoviesList />
+      </main>
+    </HomePageStyled>
+  );
 };
 
 export default HomePage;
