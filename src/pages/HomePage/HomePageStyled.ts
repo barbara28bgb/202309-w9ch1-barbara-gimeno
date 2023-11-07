7d849f1fe05b08505eb18119bcb5931c6b7d15c6
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 22:49:39 +0100

    Add styles to HomePage

--- src/pages/HomePage/HomePageStyled.ts
+++ src/pages/HomePage/HomePageStyled.ts
@@ -2,11 +2,17 @@ import styled from "styled-components";
 
 const HomePageStyled = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.color.mainBackground};
 
   .home-page {
     &__title {
       padding: 20px 0 0 0;
       background-color: ${({ theme }) => theme.color.mainBackground};
+      font-size: 2.4rem;
+      text-align: center;
     }
   }
 `;
