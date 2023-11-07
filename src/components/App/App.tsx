a4a50f96fe18b975b7ba3467c09bc86fa59992e5
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 16:11:24 +0100

    Change App routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
 
 const App = (): React.ReactElement => {
-  const movies = useAppSelector((state) => {
-    return state.moviesState;
-  });
-
-  const dispatch = useAppDispatch();
-
   return (
-        <Routes>
-          <Route path="/films" element={<HomePage />} />
-          <Route path="/" element={<Navigate to= "*home" /> }
-        </Routes>
+    <Routes>
+      <Route path="/films" element={<HomePage />} />
+      <Route path="/" element={<Navigate to="/films" />} />
+    </Routes>
   );
 };
 
