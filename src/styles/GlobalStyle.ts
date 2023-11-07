6cb37db54694d5ba34649a239a51d27c7123d7f6
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 19:02:26 +0100

    Add GlobalStyle.ts

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  ::after,
+  ::before {
+    box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  body {
+    margin: 0;
+    padding:0;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.typography.mainFontFamily};
+    color: ${({ theme }) => theme.color.mainFont};
+    background-color: ${({ theme }) => theme.color.mainBackground};
+  }
+`;
+
+export default GlobalStyle;
