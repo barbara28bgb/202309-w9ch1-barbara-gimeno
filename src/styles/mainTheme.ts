c3207671a10b9b7c0620292de2baf647e8ccf528
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 19:04:48 +0100

    Add mainTheme.ts

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  boderRadius: "10px",
+  color: {
+    cardBackground: "#ddf4ca",
+    mainBackground: "#efeee6;",
+    mainFont: "#393939eb",
+  },
+  typography: {
+    mainFontFamily: "'Roboto', sans-serif",
+    propertiesSize: "1,1875rem",
+    titleSize: "2.5rem",
+    headerSize: "1.7rem",
+  },
+};
+
+export default mainTheme;
