99472dba4c7193e2bca64687ea29114d7ed089ce
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 19:32:57 +0100

    Add ThemeProvider and GlobalStyle to main

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,19 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 import { store } from "./store";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import mainTheme from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle>
+            <App />
+          </GlobalStyle>
+        </ThemeProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
