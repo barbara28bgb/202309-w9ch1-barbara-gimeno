44f9ffc68819f1947eb08bf712321c96ed887126
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-07 22:55:07 +0100

    Add styles to MovieCard

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,17 +1,25 @@
 import { MovieStructure } from "../../store/features/movieSlice/types";
+import MovieCardStyled from "./MovieCardStyled";
 
 interface MoviesCardProps {
   movie: MovieStructure;
 }
 
 const MovieCard = ({
-  movie: { name, year },
+  movie: { name, year, image },
 }: MoviesCardProps): React.ReactElement => {
   return (
-    <article>
-      <h2 className="movies"> {name}</h2>
-      <span>{year}</span>
-    </article>
+    <MovieCardStyled className="movie-container">
+      <img
+        className="movie-container__image"
+        src={image}
+        alt={name}
+        width={380}
+        height={500}
+      />
+      <h2 className="movie-container__title"> {name}</h2>
+      <span className="movie-container__year">{year}</span>
+    </MovieCardStyled>
   );
 };
 
