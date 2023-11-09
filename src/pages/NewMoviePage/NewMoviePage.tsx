3ee0988e34d259d4e3de8ade99b2338658c9c003
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-09 21:29:38 +0100

    Add NewMoviePage

--- src/pages/NewMoviePage/NewMoviePage.tsx
+++ src/pages/NewMoviePage/NewMoviePage.tsx
@@ -1,5 +1,12 @@
-const newMoviePage = (): React.ReactElement => {
-  return <h1>New Film</h1>;
+import MovieForm from "../../components/Form/Form";
+
+const NewMoviePage = (): React.ReactElement => {
+  return (
+    <>
+      <h1>Añade una nueva película</h1>
+      <MovieForm></MovieForm>
+    </>
+  );
 };
 
-export default newMoviePage;
+export default NewMoviePage;
