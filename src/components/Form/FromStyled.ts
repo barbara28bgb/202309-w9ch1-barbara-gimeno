7b801d156d3925a75a8ff2909832442f736e10e5
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-09 22:37:29 +0100

    Add form with styles

---
+++ src/components/Form/FromStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const FormStyled = styled.form`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  box-shadow: 6px 11px 11px 0px;
+  border-radius: 33px;
+  width: 400px;
+  padding: 20px;
+`;
+
+export default FormStyled;
