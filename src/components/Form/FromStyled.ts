cfa3112205e31338445347aebc1c30752e837c8e
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-10 13:00:14 +0100

    Add styles to Form

--- src/components/Form/FromStyled.ts
+++ src/components/Form/FromStyled.ts
@@ -1,15 +1,38 @@
 import styled from "styled-components";
 
 const FormStyled = styled.form`
-  background-color: white;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  box-shadow: 6px 11px 11px 0px;
-  border-radius: 33px;
-  width: 400px;
+  gap: 30px;
   padding: 20px;
+  font-family: ${({ theme }) => theme.typography.mainFontFamily};
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    background-color: ${({ theme }) => theme.color.cardBackground};
+    padding: 5px;
+
+    &__label {
+      font-size: 1.4rem;
+      margin-bottom: 12px;
+    }
+
+    &__input {
+      padding: 10px 0;
+      color: ${({ theme }) => theme.color.mainBackground};
+      font-size: 1rem;
+      border: solid;
+      border-color: #393939eb;
+      border-radius: 8px;
+    }
+
+    &__box {
+      margin-top: 10px;
+    }
+  }
 `;
 
 export default FormStyled;
